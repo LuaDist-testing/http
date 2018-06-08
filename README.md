c638416d061042050717672ad108a7e3a52a9c1b
LunaCI <lunaci@luadist.org>
2018-06-08 00:51:16 +0000

    Update to version 0.1-0

---
+++ README.md
@@ -0,0 +1,92 @@
+# HTTP library for Lua.
+
+## Features
+
+  - Optionally asynchronous (including DNS lookups and TLS)
+  - Supports HTTP(S) version 1.0, 1.1 and 2
+  - Functionality for both client and server
+  - Websockets
+  - Compatible with Lua 5.1, 5.2, 5.3 and [LuaJIT](http://luajit.org/)
+
+
+## Documentation
+
+Can be found at [https://daurnimator.github.io/lua-http/](https://daurnimator.github.io/lua-http/)
+
+
+## Status
+
+[![Build Status](https://travis-ci.org/daurnimator/lua-http.svg)](https://travis-ci.org/daurnimator/lua-http)
+[![Coverage Status](https://coveralls.io/repos/daurnimator/lua-http/badge.svg?branch=master&service=github)](https://coveralls.io/github/daurnimator/lua-http?branch=master)
+
+
+# Installation
+
+It's recommended to install lua-http by using [luarocks](https://luarocks.org/).
+This will automatically install run-time lua dependencies for you.
+
+    $ luarocks install http
+
+## Dependencies
+
+  - [cqueues](http://25thandclement.com/~william/projects/cqueues.html) >= 20161214
+  - [luaossl](http://25thandclement.com/~william/projects/luaossl.html) >= 20161208
+  - [basexx](https://github.com/aiq/basexx/) >= 0.2.0
+  - [lpeg](http://www.inf.puc-rio.br/~roberto/lpeg/lpeg.html)
+  - [lpeg_patterns](https://github.com/daurnimator/lpeg_patterns) >= 0.3
+  - [fifo](https://github.com/daurnimator/fifo.lua)
+
+To use gzip compression you need **one** of:
+
+  - [lzlib](https://github.com/LuaDist/lzlib) or [lua-zlib](https://github.com/brimworks/lua-zlib)
+
+If using lua < 5.3 you will need
+
+  - [compat-5.3](https://github.com/keplerproject/lua-compat-5.3) >= 0.3
+
+If using lua 5.1 you will need
+
+  - [luabitop](http://bitop.luajit.org/) (comes [with LuaJIT](http://luajit.org/extensions.html)) or a [backported bit32](https://luarocks.org/modules/siffiejoe/bit32)
+
+### For running tests
+
+  - [luacheck](https://github.com/mpeterv/luacheck)
+  - [busted](http://olivinelabs.com/busted/)
+  - [luacov](https://keplerproject.github.io/luacov/)
+
+
+# Development
+
+## Getting started
+
+  - Clone the repo:
+    ```
+    $ git clone https://github.com/daurnimator/lua-http.git
+    $ cd lua-http
+    ```
+
+  - Install dependencies
+    ```
+    $ luarocks install --only-deps http-scm-0.rockspec
+    ```
+
+  - Lint the code (check for common programming errors)
+    ```
+    $ luacheck .
+    ```
+
+  - Run tests and view coverage report ([install tools first](#for-running-tests))
+    ```
+    $ busted -c
+    $ luacov && less luacov.report.out
+    ```
+
+  - Install your local copy:
+    ```
+    $ luarocks make http-scm-0.rockspec
+    ```
+
+
+## Generating documentation
+
+Documentation is written in markdown and intended to be consumed by [pandoc](http://pandoc.org/). See the `doc/` directory for more information.
---
+++ http/client.lua
@@ -0,0 +1,105 @@
+local ca = require "cqueues.auxlib"
+local cs = require "cqueues.socket"
+local http_tls = require "http.tls"
+local connection_common = require "http.connection_common"
+local onerror = connection_common.onerror
+local new_h1_connection = require "http.h1_connection".new
+local new_h2_connection = require "http.h2_connection".new
+local openssl_ssl = require "openssl.ssl"
+local openssl_ctx = require "openssl.ssl.context"
+local openssl_verify_param = require "openssl.x509.verify_param"
+
+local EOF = require "lpeg".P(-1)
+local IPv4address = require "lpeg_patterns.IPv4".IPv4address
+local IPv6addrz = require "lpeg_patterns.IPv6".IPv6addrz
+local IPaddress = (IPv4address + IPv6addrz) * EOF
+
+-- Create a shared 'default' TLS context
+local default_ctx = http_tls.new_client_context()
+
+local function negotiate(s, options, timeout)
+	s:onerror(onerror)
+	local tls = options.tls
+	local version = options.version
+	if tls then
+		local ctx = options.ctx or default_ctx
+		local ssl = openssl_ssl.new(ctx)
+		local ip = options.host and IPaddress:match(options.host)
+		if options.sendname ~= nil then
+			if options.sendname then -- false indicates no sendname wanted
+				ssl:setHostName(options.sendname)
+			end
+		elseif options.host and not ip then
+			ssl:setHostName(options.host)
+		end
+		if http_tls.has_alpn then
+			if version == nil then
+				ssl:setAlpnProtos({"h2", "http/1.1"})
+			elseif version == 1.1 then
+				ssl:setAlpnProtos({"http/1.1"})
+			elseif version == 2 then
+				ssl:setAlpnProtos({"h2"})
+			end
+		end
+		if version == 2 then
+			ssl:setOptions(openssl_ctx.OP_NO_TLSv1 + openssl_ctx.OP_NO_TLSv1_1)
+		end
+		if options.host and http_tls.has_hostname_validation then
+			local params = openssl_verify_param.new()
+			if ip then
+				params:setIP(options.host)
+			else
+				params:setHost(options.host)
+			end
+			-- Allow user defined params to override
+			local old = ssl:getParam()
+			old:inherit(params)
+			ssl:setParam(old)
+		end
+		local ok, err, errno = s:starttls(ssl, timeout)
+		if not ok then
+			return nil, err, errno
+		end
+	end
+	if version == nil then
+		local ssl = s:checktls()
+		if ssl then
+			if http_tls.has_alpn and ssl:getAlpnSelected() == "h2" then
+				version = 2
+			else
+				version = 1.1
+			end
+		else
+			-- TODO: attempt upgrading http1 to http2
+			version = 1.1
+		end
+	end
+	if version < 2 then
+		return new_h1_connection(s, "client", version)
+	elseif version == 2 then
+		return new_h2_connection(s, "client", options.h2_settings)
+	else
+		error("Unknown HTTP version: " .. tostring(version))
+	end
+end
+
+local function connect(options, timeout)
+	local s, err, errno = ca.fileresult(cs.connect {
+		family = options.family;
+		host = options.host;
+		port = options.port;
+		path = options.path;
+		sendname = false;
+		v6only = options.v6only;
+		nodelay = true;
+	})
+	if s == nil then
+		return nil, err, errno
+	end
+	return negotiate(s, options, timeout)
+end
+
+return {
+	negotiate = negotiate;
+	connect = connect;
+}
